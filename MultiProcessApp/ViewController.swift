3ad7a354fe2f50e63efb59575ef19a01a0599d7d
Dávid Maksa <maksadavid@gmail.com>
2020-03-19 07:49:30 +0100

    Initial Commit

---
+++ MultiProcessApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiProcessApp
+//
+//  Created by David Maksa on 19.03.20.
+//  Copyright © 2020 David Maksa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
